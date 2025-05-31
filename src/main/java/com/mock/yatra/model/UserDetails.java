d86fdc4c3f5f81ad1cb583366d5ec4ee0189cfb3
rajanmishra450 <rajanmishra450@gmail.com>
2025-05-31 23:27:29 +0530

    Adding Json property changes

--- src/main/java/com/mock/yatra/model/UserDetails.java
+++ src/main/java/com/mock/yatra/model/UserDetails.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
@@ -23,6 +24,8 @@ public class UserDetails {
     private String password;
     private String token;  
     @JsonFormat(pattern = "dd-MM-yyyy")
+    @JsonProperty(value = "dateOfBirth")
     private LocalDate DOB;
+    @JsonProperty(value = "email")
     private String emailId;
 }
